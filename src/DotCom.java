21b610ffb501935e4bf4b8ed61a926699718e4ff
officialdun <49313809+officialdun@users.noreply.github.com>
2023-10-08 18:26:04 -0700

    debugging

--- src/DotCom.java
+++ src/DotCom.java
@@ -4,6 +4,7 @@ public class DotCom {
     int numOfHits = 0;
     DotComTester tester = new DotComTester();
     Boolean isAlive = true;
+    int[] hits = new int[3];
 
    /* int startingPoint = (int)(Math.random()*5);*/
 
@@ -11,16 +12,40 @@ public class DotCom {
         int Guess = Integer.parseInt(userGuesses);
         String result = "miss";
 
+
+        int ch = 0;
+
         for (int cell : locationsCells) {
             if (Guess == cell) {
                 result = "hit";
+
+
+                /*for (int g: hits){
+                    if(Guess==g){
+                       // break;
+                    }else{
+                        hits[numOfHits] = Guess;
+                    }
+                }*/
+
+               // locationsCells
                 numOfHits++;
                 break;
             }
+
+
+
+
         }
 
         if (numOfHits == locationsCells.length) {
             result = "kill";
+
+            for (int g: hits){
+                System.out.print(g+" ");
+            }
+            System.out.println();
+
             System.out.println("You took "+tester.numOfGuesses+" guesses");
         }
         System.out.println(result);
